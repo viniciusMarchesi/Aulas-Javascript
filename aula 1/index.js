ce5b7c86d1ebe93c903ae77f62121bf25c70d659
Vinicius Marchesi <viniciusmarchesi2712@gmail.com>
2024-01-05 16:04:15 -0300

    Aula 1
    Aula 1 de JavaScript

---
+++ aula 1/index.js
@@ -0,0 +1,24 @@
+/** Aula sobre Variaveis
+ * existem duas maneiras para criar variaveis em javascript, Let é uma forma, Var é outra,
+ * são  praticamente a mesma coisa.
+ * Não podemos criar variaveis com palavras reservadas.
+ * variaveis precisam ter nomes significativos.
+ * não pode começar o nome de uma variavel com numero.
+ * não pode conter espaços ou traços.
+ * utilizamos camelCase quando queremos criar uma variavel composta exemplo:
+ * nomeCliente  a primeira palavras se inicia com letra minuscula e a segunda em maiuscula.
+ * Case-sensitive
+ * Não podemos redeclarar variaveis com let
+ * não utilize var, sempre utilize let.
+ */
+
+let nome = 'João'; // string
+let nomeCompletoDoCliente = 'Vinicius Silva';
+console.log(nomeCompletoDoCliente)
+
+
+console.log( nome,'nasceu em 1984.');
+console.log('Em 2000', nome, 'conheceu Maria.');
+console.log( nome,'casou-se com maria em 2012.');
+console.log('Maria teve um filho com', nome, 'em 2015.');
+console.log('O filho de', nome, 'se chama Eduardo.');
---
+++ aula 2/index.js
@@ -0,0 +1,8 @@
+// Aula Sobre constantes
+// Variavel constante é uma variavel que não muda seu valor no caso.
+// segue as mesmas regras da aula 1.
+// Não pode modificar o valor de uma constante.
+// NÃO UTILIZE VAR, UTILIZE CONST.
+
+const nome = 'João'
+console.log(nome)
